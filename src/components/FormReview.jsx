6948eb94a9ee4a88f59e6aaff46cc8d695de156d
InstaCodex <chandrafajar356@gmail.com>
2024-04-16 21:10:07 +0700

    Update FormReview.jsx

--- src/components/FormReview.jsx
+++ src/components/FormReview.jsx
@@ -7,7 +7,7 @@ export default function FormReview() {
         <h3>Add Comment Reveiw </h3>
         <form action="">
           <div className="inputStyle" style={{ gap: '10px' }}>
-            <textarea placeholder="write a message..." className="formInput" id="message" rows={8} defaultValue={''} />
+            <textarea placeholder="write a comment..." className="formInput" id="message" rows={8} defaultValue={''} />
           </div>
           <button type="button" className="btn" style={{ background: '#0f486c', color: '#fff', width: '120px' }}>
             Submit
